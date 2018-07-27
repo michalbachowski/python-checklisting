0d147b97a3e6a37ff9066c0ba3b9bd7e12522200
Michał Bachowski <michal@bachowski.pl>
2018-07-27 11:15:30 +0200

    Refactor sample checklist according to recent changes

--- tests/fixtures/sample_checklists/simple.py
+++ tests/fixtures/sample_checklists/simple.py
@@ -1,11 +1,12 @@
 from pathlib import Path
-from typing import Dict, Any
+
+from checklisting.loaders import BaseChecklistSourceConfiguration
+from checklisting.loaders.pyspd import PySPDChecklistProvider
 from checklisting.task import Checklist
-from checklisting.interfaces.common.loaders.pyspd import PySPDChecklistProvider
 from checklisting.tasks.path import FileExistsTask
 
 
 class SimpleChecklistProvider(PySPDChecklistProvider):
 
-    def get_checklist(self, configuration: Dict[str, Any]) -> Checklist:
+    def get_checklist(self, configuration: BaseChecklistSourceConfiguration) -> Checklist:
         return Checklist("simple_checklist", [FileExistsTask(Path(__file__))])
