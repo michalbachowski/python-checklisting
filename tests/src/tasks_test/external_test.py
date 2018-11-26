a23c932f720535d1186728878cc5deeda7457cb9
Michał Bachowski <michal@bachowski.pl>
2018-11-26 08:38:54 +0100

    Add tests for ExternalChecklistResponseValidator

--- tests/src/tasks_test/external_test.py
+++ tests/src/tasks_test/external_test.py
@@ -1,13 +1,15 @@
 import asyncio
 from typing import Awaitable, Callable, Optional
 
+import aiohttp
 import asynctest
 import mock
 
 from checklisting.result import BaseTaskResult, MultiTaskResult, TaskResult
 from checklisting.result.status import TaskResultStatus
 from checklisting.serializer.json import JsonSerializer
-from checklisting.tasks.external import ExternalChecklistResponseValidator, ExternalChecklistTask
+from checklisting.tasks.external import (ExternalChecklistResponseValidator,
+                                         ExternalChecklistTask)
 from checklisting.testing import setup_tcp_server
 
 
@@ -55,4 +57,31 @@ Connection: Closed
         res = TaskResult(TaskResultStatus.SUCCESS, 'test_message')
         await self._do_test(
             res,
-            MultiTaskResult(TaskResultStatus.FAILURE, mock.ANY, [TaskResult(TaskResultStatus.INFO, self._serializer.dumps(res))]))
+            MultiTaskResult(TaskResultStatus.FAILURE, mock.ANY,
+                            [TaskResult(TaskResultStatus.INFO, self._serializer.dumps(res))]))
+
+
+class ExternalChecklistResponseValidatorTest(asynctest.TestCase):
+
+    def setUp(self) -> None:
+        self._validator = ExternalChecklistResponseValidator()
+        self._response = mock.Mock(aiohttp.ClientResponse)
+        self._response.status = 200
+        self._response_dict = dict(test_key='test_value')
+        self._response.json = asynctest.CoroutineMock(return_value=self._response_dict)
+        self._response.text = asynctest.CoroutineMock(return_value='test_text')
+
+    async def test_for_status_200_returns_parsed_json_as_is(self) -> None:
+        self.assertIs(self._response_dict, await self._validator.validate(self._response))
+
+    async def test_for_status_other_than_200_returns_response_containing_response_test(self) -> None:
+        self._response.status = 201
+
+        result = await self._validator.validate(self._response)
+
+        self.assertEqual(result.status, TaskResultStatus.FAILURE)
+        self.assertIn('Call to external checklist', result.message)
+
+        inner_result = result.results[0]
+        self.assertEqual(inner_result.status, TaskResultStatus.INFO)
+        self.assertEqual(inner_result.message, 'test_text')
