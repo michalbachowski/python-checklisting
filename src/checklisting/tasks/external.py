87be6813d30439a68b486e26dd5fc428962e2ca7
Michał Bachowski <michal@bachowski.pl>
2018-11-28 22:49:37 +0100

    Build final ExternalTask results out of received results

--- src/checklisting/tasks/external.py
+++ src/checklisting/tasks/external.py
@@ -2,9 +2,10 @@ from typing import Any, Optional, Union
 
 from checklisting.extras import import_module
 from checklisting.result import BaseTaskResult, MultiTaskResult, TaskResult
+from checklisting.result.builder import MultiTaskResultBuilder
 from checklisting.result.status import TaskResultStatus
 from checklisting.serializer.json import JsonDeserializer
-from checklisting.tasks.http import (BaseHttpTaskResponseValidator, HttpMethod, HttpTask)
+from checklisting.tasks.http import BaseHttpTaskResponseValidator, HttpMethod, HttpTask
 
 aiohttp = import_module('aiohttp')
 yarl = import_module('yarl')
@@ -12,13 +13,14 @@ yarl = import_module('yarl')
 
 class ExternalChecklistResponseValidator(BaseHttpTaskResponseValidator):
 
-    def __init__(self) -> None:
+    def __init__(self, result_builder: Optional[MultiTaskResultBuilder] = None) -> None:
         super().__init__()
         self._deserializer = JsonDeserializer()
+        self._result_builder = result_builder or MultiTaskResultBuilder()
 
     async def validate(self, response: aiohttp.ClientResponse) -> BaseTaskResult:
         if response.status == 200:
-            return await response.json(loads=self._deserializer.loads)
+            return self._result_builder.of_results(await response.json(loads=self._deserializer.loads))
         else:
             r = MultiTaskResult(TaskResultStatus.FAILURE,
                                 f'Call to external checklist [{response.real_url}] failed. See subtasks for details.',
