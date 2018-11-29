f3eeec88f6114187565a1dbd5d68ce4613a8ba9e
Michał Bachowski <michal@bachowski.pl>
2018-11-29 09:54:44 +0100

    Add IP to external checklist results

--- src/checklisting/tasks/external.py
+++ src/checklisting/tasks/external.py
@@ -3,9 +3,12 @@ from typing import Any, Optional, Union
 from checklisting.extras import import_module
 from checklisting.result import BaseTaskResult, MultiTaskResult, TaskResult
 from checklisting.result.builder import MultiTaskResultBuilder
+from checklisting.result.message.builder import \
+    PrefixedTaskResultMessageBuilder
 from checklisting.result.status import TaskResultStatus
 from checklisting.serializer.json import JsonDeserializer
-from checklisting.tasks.http import BaseHttpTaskResponseValidator, HttpMethod, HttpTask
+from checklisting.tasks.http import (BaseHttpTaskResponseValidator, HttpMethod,
+                                     HttpTask)
 
 aiohttp = import_module('aiohttp')
 yarl = import_module('yarl')
@@ -13,10 +16,11 @@ yarl = import_module('yarl')
 
 class ExternalChecklistResponseValidator(BaseHttpTaskResponseValidator):
 
-    def __init__(self, result_builder: Optional[MultiTaskResultBuilder] = None) -> None:
+    def __init__(self, source: str, result_builder: Optional[MultiTaskResultBuilder] = None) -> None:
         super().__init__()
         self._deserializer = JsonDeserializer()
-        self._result_builder = result_builder or MultiTaskResultBuilder()
+        self._result_builder = result_builder or \
+            MultiTaskResultBuilder(message_builder=PrefixedTaskResultMessageBuilder(source))
 
     async def validate(self, response: aiohttp.ClientResponse) -> BaseTaskResult:
         if response.status == 200:
@@ -34,4 +38,4 @@ class ExternalChecklistTask(HttpTask):
                  url: Union[str, yarl.URL],
                  validator: Optional[BaseHttpTaskResponseValidator] = None,
                  **kwargs: Any) -> None:
-        super().__init__(HttpMethod.GET, url, validator or ExternalChecklistResponseValidator(), **kwargs)
+        super().__init__(HttpMethod.GET, url, validator or ExternalChecklistResponseValidator(url), **kwargs)
